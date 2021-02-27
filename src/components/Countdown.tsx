4916ed855ed59f607eb1480b8a98af10840eda37
Leandro Roberto <leandro.roberto@dextra-sw.com>
2021-02-27 17:23:38 -0300

    ChallengeBox styles

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,8 +3,11 @@ import {useState, useEffect} from 'react';
 
 export function Countdown() {
     
-    const [time, setTime] = useState(25 * 60) ;
-    const [active, setActive] = useState(false);
+    let countdownTimeout : NodeJS.Timeout;
+
+    const [time, setTime] = useState(0.1 * 60) ;
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
     
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -13,16 +16,27 @@ export function Countdown() {
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown(){
-        setActive(true);
+        setIsActive(true);
+    }
+
+    function resetCountdown(){
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setTime(0.1 *60);
     }
 
     useEffect(() => {
-        if (active && time > 0){
-            setTimeout(() => {
+        if (isActive && time > 0){
+           countdownTimeout = setTimeout(() => {
                 setTime(time - 1)
             },1000)
         }
-    },[active,time]);
+        else if (isActive && time == 0){
+            console.log("finalizou");
+            setHasFinished(true);
+            setIsActive(false);
+        }
+    },[isActive,time]);
 
     return (
         <div>
@@ -38,14 +52,42 @@ export function Countdown() {
                 </div>
             </div>
 
-            <button 
-                type="button" 
-                className={styles.countdownButton}
-                onClick={startCountdown}    
-            >
-                Iniciar um ciclo
-            </button>
-
+            {
+                hasFinished ? (
+                <button
+                    disabled                
+                    className={styles.countdownButton}                
+                >
+                    Ciclo encerrado
+                </button>
+                )
+                :
+                (
+                    <>
+                        {
+                        isActive ? (
+                            <button
+                                type="button"
+                                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                                onClick={resetCountdown}
+                            >
+                                Abandonar ciclo
+                            </button>
+                            ) 
+                            :
+                            (
+                                <button
+                                    type="button"
+                                    className={styles.countdownButton}
+                                    onClick={startCountdown}
+                                >
+                                    Iniciar um ciclo
+                                </button>
+                            )
+                        }
+                    </>
+                )
+            }            
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import styles from './../styles/pages/Home.module.css';
 
+import Head from 'next/head';
 
 import { Profile } from '../components/Profile';
 import {ExperienceBar} from './../components/ExperienceBar';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
-
-import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -24,7 +24,7 @@ export default function Home() {
             <Countdown />    
         </div>
         <div>
-          
+          <ChallengeBox />
         </div>        
       </section>
     </div>
